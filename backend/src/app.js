7a1a1ac57345fda6c7f2fc593e4f6cffadf63f38
aryan-494 <aryanmishra49712@gmail.com>
2026-01-03 10:17:15 +0530

    Fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,9 +7,12 @@ const app = express()
 // ✅ CORS (frontend → backend)
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
   })
-)
+);
+
 
 // ✅ Body parser
 app.use(express.json())
